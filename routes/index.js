eacd293f9e3a9f6afda45fb32ff729760110ae4d
Tanmay <tanmaykacker40@gmail.com>
2015-10-18 18:52:16 +0530

    Skeleton of Comments System
    Nodes.js, Express, MongoDB, Mongoose, EJS, Stylus

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var mongoose=require('mongoose');
+var Comment=mongoose.model('Comment');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	Comment.find(function(err,comments,count){
+  		res.render('index', {
+  		 title: 'Comments with Mongoose',
+  		 comments:comments 
+		});
+  	});
+});
+
+router.post('/create',function(req,res){
+	new Comment({
+		username : req.body.username,
+		content : req.body.content,
+		created : Date.now()
+	}).save(function(err,comment,count){
+		console.log(comment);
+		res.redirect('/');
+	});
+});
+
+
+module.exports = router;
